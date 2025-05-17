7738cce2895470e7e23ea217d46107791b44d5bc
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-17 14:22:28 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const app = express();
-
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
+app.set(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
