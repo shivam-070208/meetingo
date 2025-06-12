f969e5151379380765a8b413dd32616f85006b74
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-06-12 11:58:01 +0530

    final

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,29 +1,32 @@
-const newmeetbtn = document.querySelector('#newmeet');
+const newmeetbtn = document.querySelectorAll('#newmeet');
 const joinmeet = document.querySelector('.joinmeet');
 
 let creating = false;
-newmeetbtn.addEventListener('click', async (e) => {
-    if (creating) return;
-    creating = true;
-    let value =e.target.textContent
-    e.target.textContent = "creating.....";
-    
-    try {
-        const response = await fetch('/api/create', { method: "POST" });
-        if (!response.ok) throw new Error("Failed to create meeting");
+newmeetbtn.forEach((newmeetbtn)=>{
+
+    newmeetbtn.addEventListener('click', async (e) => {
+        if (creating) return;
+        creating = true;
+        let value =e.target.textContent
+        e.target.textContent = "creating.....";
         
-        const data = await response.json(); 
-         e.target.textContent =value;
-        creating = false;
-       
-        window.location.href = `${window.origin}/meet?id=${data.data}`;  
-    } catch (error) {
-        console.error(error);
-        e.target.textContent = "Error! Try again.";
-        e.target.textContent =value;
-        creating = false;
-    }
-});
+        try {
+            const response = await fetch('/api/create', { method: "POST" });
+            if (!response.ok) throw new Error("Failed to create meeting");
+            
+            const data = await response.json(); 
+             e.target.textContent =value;
+            creating = false;
+           
+            window.location.href = `${window.origin}/meet?id=${data.data}`;  
+        } catch (error) {
+            console.error(error);
+            e.target.textContent = "Error! Try again.";
+            e.target.textContent =value;
+            creating = false;
+        }
+    });
+})
 joinmeet.addEventListener('click', async (e) => {
     if (creating) return;
     creating = true;
